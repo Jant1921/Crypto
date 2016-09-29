0e1bf937cc38e8818694bfe4d5ddcde5315452fb
Jose Ruiz <jruizj6@hotmail.com>
2016-09-29 17:28:40 -0600

    setters y getters creados

--- CriptoSystem/Datos.cs
+++ CriptoSystem/Datos.cs
@@ -15,5 +15,40 @@ namespace CriptoSystem
         public string nombreAlgoritmo;
         public string valorCodificacion;
         public Alfabeto alfabeto;
+
+        public string Fecha {
+            get { return fecha; }
+            set { fecha = DateTime.Now.ToString(); }
+        }
+
+        public string FraseOriginal {
+            get { return fraseOriginal; }
+            set { FraseOriginal = value; }
+        }
+
+        public string FraseResultado{
+            get { return fraseResultado; }
+            set { fraseResultado= value; }
+        }
+
+        public bool Codifica{
+            get { return codifica; }
+            set { codifica = value; }
+        }
+
+        public string NombreAlgoritmo{
+            get { return nombreAlgoritmo; }
+            set { nombreAlgoritmo = value; }
+        }
+
+        public string ValorCodificacion {
+            get { return valorCodificacion; }
+            set { valorCodificacion = value; }
+        }
+
+        public Alfabeto Alfabeto {
+            get { return alfabeto; }
+            set { alfabeto = value; }
+        }
     }
 }
